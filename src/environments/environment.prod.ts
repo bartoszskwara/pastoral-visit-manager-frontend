6326c1e00729a235643911b2f5be38cbbfae6f9c
Bartosz Skwara <bartoszskwara95@gmail.com>
2018-08-24 18:10:51 +0200

    Add prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  server: {
+    url: 'http://localhost:8090'
+  },
+  dateFormat: "YYYY-MM-DD HH:mm:ss ZZ",
+  pastoralVisit: {
+    status: {
+      completed: '+',
+      refused: '-',
+      absent: '?',
+      individually: 'ind.',
+      not_requested: 'x'
+    }
+  }
 };
